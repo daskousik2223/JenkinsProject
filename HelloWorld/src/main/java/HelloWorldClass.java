e915fe6c28978d26495f4c3030abf73b5d8dc98e
daskousik2223 <das.kousik2223@gmail.com>
2020-05-18 12:12:18 -0400

    Updated 5/18 12:12 PM

--- HelloWorld/src/main/java/HelloWorldClass.java
+++ HelloWorld/src/main/java/HelloWorldClass.java
@@ -2,7 +2,7 @@
 public class HelloWorldClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World" + "Jenkins is working");
 
 	}
 
